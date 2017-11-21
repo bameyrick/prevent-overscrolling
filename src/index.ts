92cc4c9e35e5270e0931916ea9f4cb4a6344c09e
bameyrick <bameyrick@gmail.com>
2017-11-21 17:01:19 +0000

    Improve overscrolling to prevent jitter

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-import { USER_SCROLL_EVENTS } from './user-scroll-events';
+import { USER_SCROLL_EVENTS, USER_SCROLL_KEYBOARD_EVENTS } from './user-scroll-events';
+import { IPosition } from './models';
+import { EKeyCodes } from './keycodes';
+
+let touchStart: IPosition;
+let hasFocus = false;
 
 export function PreventOverScrolling(scrollableArea: HTMLElement): void {
 	setEvents(true, scrollableArea);
@@ -8,22 +13,82 @@ export function ReEnableOverScrolling(scrollableArea: HTMLElement): void {
 	setEvents(false, scrollableArea);
 }
 
-function setEvents(enable: boolean, element: HTMLElement): void {
-	USER_SCROLL_EVENTS.forEach(event => {
+function setEvents(enable: boolean, scrollableArea: HTMLElement): void {
+	USER_SCROLL_EVENTS.forEach(eventType => {
 		if (enable) {
-			element.addEventListener(event, () => handleOverScroll(element));
+			scrollableArea.addEventListener(eventType, event => handleOverScroll(event, scrollableArea));
 		} else {
-			element.removeEventListener(event, () => handleOverScroll(element));
+			scrollableArea.removeEventListener(eventType, event => handleOverScroll(event, scrollableArea));
 		}
 	});
+
+	if (enable) {
+		scrollableArea.addEventListener('touchstart', handleTouchStart);
+		window.addEventListener('keydown', event => handleOverScroll(event, scrollableArea));
+		window.addEventListener('click', event => handleClick(event, scrollableArea));
+	} else {
+		scrollableArea.removeEventListener('touchstart', handleTouchStart);
+		window.removeEventListener('keydown', event => handleOverScroll(event, scrollableArea));
+		window.removeEventListener('click', event => handleClick(event, scrollableArea));
+	}
 }
 
-function handleOverScroll(element: HTMLElement): void {
-	const scrollTop = element.scrollTop;
+function handleTouchStart(event: TouchEvent): void {
+	const touch = event.touches[0];
+	touchStart = {
+		x: touch.clientX,
+		y: touch.clientY,
+	};
+}
 
-	if (scrollTop === 0) {
-		element.scrollTop = 1;
-	} else if (scrollTop + element.offsetHeight === element.scrollHeight) {
-		element.scrollTop = scrollTop - 1;
+function handleOverScroll(event: Event, scrollableArea: HTMLElement): void {
+	const eventType = event.type;
+
+	if (eventType !== 'mousedown') {
+		const scrollY = scrollableArea.scrollTop;
+		let deltaY: number | undefined;
+
+		if (eventType === 'keydown') {
+			const tagName = (<HTMLElement>event.srcElement).tagName.toLowerCase();
+			const keyCode = (<KeyboardEvent>event).keyCode;
+			if (hasFocus && tagName !== 'input' && tagName !== 'textarea' && USER_SCROLL_KEYBOARD_EVENTS.includes(keyCode)) {
+				switch (keyCode) {
+					case EKeyCodes.Up:
+					case EKeyCodes.PageUp:
+					case EKeyCodes.Home:
+						deltaY = -1;
+						break;
+					default:
+						deltaY = 1;
+				}
+			}
+		} else if (eventType === 'touchmove') {
+			deltaY = touchStart.y - (<TouchEvent>event).touches[0].clientY;
+		} else {
+			deltaY = (<MouseWheelEvent>event).deltaY;
+		}
+
+		if (deltaY) {
+			// Handle -0 also
+			if (deltaY.toString().includes('-')) {
+				if (scrollY === 0) {
+					event.preventDefault();
+				}
+			} else {
+				if (scrollY + scrollableArea.offsetHeight === scrollableArea.scrollHeight) {
+					event.preventDefault();
+				}
+			}
+		}
+	}
+}
+
+function handleClick(event: Event, scrollableArea: HTMLElement): void {
+	const source = <HTMLElement>event.srcElement;
+
+	if (source === scrollableArea || scrollableArea.contains(source)) {
+		hasFocus = true;
+	} else {
+		hasFocus = false;
 	}
 }
--- src/user-scroll-events.ts
+++ src/user-scroll-events.ts
@@ -1,1 +1,15 @@
-export const USER_SCROLL_EVENTS = ['mousedown', 'wheel', 'DOMMouseScroll', 'mousewheel', 'keyup', 'touchmove'];
+import { EKeyCodes } from './keycodes';
+
+export const USER_SCROLL_EVENTS = ['mousedown', 'wheel', 'DOMMouseScroll', 'mousewheel', 'touchmove'];
+
+export const USER_SCROLL_KEYBOARD_EVENTS = [
+	EKeyCodes.SpaceBar,
+	EKeyCodes.PageUp,
+	EKeyCodes.PageDown,
+	EKeyCodes.End,
+	EKeyCodes.Home,
+	EKeyCodes.Left,
+	EKeyCodes.Up,
+	EKeyCodes.Right,
+	EKeyCodes.Down,
+];
