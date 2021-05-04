7ab0d1ed1bfa3577f163934109a2174fad5f5bd5
Ben Meyrick <bameyrick@gmail.com>
2021-05-04 13:37:02 +0100

    Fix linting

--- src/scrollable-area.ts
+++ src/scrollable-area.ts
@@ -1,68 +1,87 @@
 import { USER_SCROLL_EVENTS, USER_SCROLL_KEYBOARD_EVENTS } from './user-scroll-events';
-import { IPosition } from './models';
-import { EKeyCodes } from './keycodes';
+import { Position } from './models';
 
 const win = window;
 
 export class ScrollableArea {
-  private touchStart: IPosition;
-  private hasFocus: boolean = false;
+  /**
+   * Reference to the position where the touch was started
+   */
+  private touchStart: Position;
 
-  constructor(public Element: HTMLElement) {}
+  /**
+   * Whether the provided element is focused
+   */
+  private focused: boolean = false;
 
-  public SetEventListeners(enable: boolean): void {
+  constructor(public element: HTMLElement) {}
+
+  /**
+   * Adds all the necessary event listeners for the given element
+   */
+  public setEventListeners(enable: boolean): void {
     USER_SCROLL_EVENTS.forEach(eventType => {
       if (enable) {
-        this.Element.addEventListener(eventType, this.handleOverScroll.bind(this));
+        this.element.addEventListener(eventType, this.handleScroll.bind(this));
       } else {
-        this.Element.removeEventListener(eventType, this.handleOverScroll.bind(this));
+        this.element.removeEventListener(eventType, this.handleScroll.bind(this));
       }
     });
 
     if (enable) {
-      this.Element.addEventListener('touchstart', this.handleTouchStart.bind(this));
-      win.addEventListener('keydown', this.handleOverScroll.bind(this));
+      this.element.addEventListener('touchstart', this.handleTouchStart.bind(this));
+      win.addEventListener('keydown', this.handleScroll.bind(this));
       win.addEventListener('click', this.handleClick.bind(this));
     } else {
-      this.Element.removeEventListener('touchstart', this.handleTouchStart.bind(this));
-      win.removeEventListener('keydown', this.handleOverScroll.bind(this));
+      this.element.removeEventListener('touchstart', this.handleTouchStart.bind(this));
+      win.removeEventListener('keydown', this.handleScroll.bind(this));
       win.removeEventListener('click', this.handleClick.bind(this));
     }
   }
 
+  /**
+   * Handles a touch start event
+   */
   private handleTouchStart(event: TouchEvent): void {
     const touch = event.touches[0];
+
     this.touchStart = {
       x: touch.clientX,
       y: touch.clientY,
     };
   }
 
+  /**
+   * Handles a click event
+   */
   private handleClick(event: Event): void {
-    const source = <HTMLElement>event.srcElement;
+    const source = event.target as HTMLElement;
 
-    if (source === this.Element || this.Element.contains(source)) {
-      this.hasFocus = true;
+    if (source === this.element || this.element.contains(source)) {
+      this.focused = true;
     } else {
-      this.hasFocus = false;
+      this.focused = false;
     }
   }
 
-  private handleOverScroll(event: Event): void {
+  /**
+   * Handles a scroll event
+   */
+  private handleScroll(event: Event): void {
     const eventType = event.type;
 
     if (eventType !== 'mousedown') {
-      const scrollY = this.Element.scrollTop;
+      const scrollY = this.element.scrollTop;
       let deltaY: number | undefined;
 
       if (eventType === 'keydown') {
-        const tagName = (<HTMLElement>event.srcElement).tagName.toLowerCase();
-        const keyCode = (<KeyboardEvent>event).keyCode;
-        if (this.hasFocus && tagName !== 'input' && tagName !== 'textarea' && USER_SCROLL_KEYBOARD_EVENTS.includes(keyCode)) {
+        const tagName = (event.target as HTMLElement).tagName.toLowerCase();
+        const keyCode = (event as KeyboardEvent).key;
+        if (this.focused && tagName !== 'input' && tagName !== 'textarea' && USER_SCROLL_KEYBOARD_EVENTS.includes(keyCode)) {
           switch (keyCode) {
-            case EKeyCodes.Up:
-            case EKeyCodes.PageUp:
-            case EKeyCodes.Home:
+            case 'ArrowUp':
+            case 'PageUp':
+            case 'Home':
               deltaY = -1;
               break;
             default:
@@ -70,9 +89,9 @@ export class ScrollableArea {
           }
         }
       } else if (eventType === 'touchmove') {
-        deltaY = this.touchStart.y - (<TouchEvent>event).touches[0].clientY;
+        deltaY = this.touchStart.y - (event as TouchEvent).touches[0].clientY;
       } else {
-        deltaY = (<MouseWheelEvent>event).deltaY;
+        deltaY = (event as WheelEvent).deltaY;
       }
 
       if (deltaY) {
@@ -82,7 +101,7 @@ export class ScrollableArea {
             event.preventDefault();
           }
         } else {
-          if (scrollY + this.Element.offsetHeight === this.Element.scrollHeight) {
+          if (scrollY + this.element.offsetHeight === this.element.scrollHeight) {
             event.preventDefault();
           }
         }
