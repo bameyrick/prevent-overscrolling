f241f79e03c6f752ba925e59defe4c3d33ee9d80
Ben Meyrick <bameyrick@gmail.com>
2021-05-04 13:17:54 +0100

    Prepare for travis and change to es2015 modules (#5)
    * Prepare for travis and change to es2015 modules
    
    * Add badges

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,24 @@ import { ScrollableArea } from './scrollable-area';
 let scrollableAreas: ScrollableArea[] = [];
 
 export function PreventOverScrolling(scrollableArea: HTMLElement): void {
-	setEvents(true, scrollableArea);
+  setEvents(true, scrollableArea);
 }
 
 export function ReEnableOverScrolling(scrollableArea: HTMLElement): void {
-	setEvents(false, scrollableArea);
+  setEvents(false, scrollableArea);
 }
 
 function setEvents(enable: boolean, element: HTMLElement): void {
-	let area = scrollableAreas.find(a => a.Element === element);
+  let area = scrollableAreas.find(a => a.Element === element);
 
-	if (!area) {
-		area = new ScrollableArea(element);
-		scrollableAreas.push(area);
-	}
+  if (!area) {
+    area = new ScrollableArea(element);
+    scrollableAreas.push(area);
+  }
 
-	area.SetEventListeners(enable);
+  area.SetEventListeners(enable);
 
-	if (!enable) {
-		scrollableAreas = scrollableAreas.filter(a => area);
-	}
+  if (!enable) {
+    scrollableAreas = scrollableAreas.filter(a => area);
+  }
 }
