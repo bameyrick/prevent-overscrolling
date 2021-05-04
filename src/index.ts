37a3560cf56161876f246487473ad39aca59d121
Ben Meyrick <bameyrick@gmail.com>
2021-05-04 13:39:29 +0100

    Fix linting (#6)

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,32 @@ import { ScrollableArea } from './scrollable-area';
 
 let scrollableAreas: ScrollableArea[] = [];
 
+/**
+ * Prevent overscrolling on a given HTMLElement
+ */
 export function PreventOverScrolling(scrollableArea: HTMLElement): void {
   setEvents(true, scrollableArea);
 }
 
+/**
+ * Re-enable overscrolling on a given HTMLElement
+ */
 export function ReEnableOverScrolling(scrollableArea: HTMLElement): void {
   setEvents(false, scrollableArea);
 }
 
+/**
+ * Sets the necessary event listenters
+ */
 function setEvents(enable: boolean, element: HTMLElement): void {
-  let area = scrollableAreas.find(a => a.Element === element);
+  let area = scrollableAreas.find(a => a.element === element);
 
   if (!area) {
     area = new ScrollableArea(element);
     scrollableAreas.push(area);
   }
 
-  area.SetEventListeners(enable);
+  area.setEventListeners(enable);
 
   if (!enable) {
     scrollableAreas = scrollableAreas.filter(a => area);
--- src/models.ts
+++ src/models.ts
@@ -1,4 +1,10 @@
-export interface IPosition {
+export interface Position {
+  /**
+   * The scroll z position
+   */
   x: number;
+  /**
+   * The scroll y position
+   */
   y: number;
 }
